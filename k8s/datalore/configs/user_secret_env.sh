84b9d153af31f8b0442fbc58c594616e5def5813
Aleksandr Borisenko <aleksandr.borisenko@jetbrains.com>
2021-10-28 16:19:41 +0300

    store env variables in a k8s secret

---
+++ k8s/datalore/configs/user_secret_env.sh
@@ -0,0 +1,22 @@
+export FRONTEND_URL=http://localhost:8080
+export DATABASES_BASE_URL=http://datalore-on-premise:8080
+
+export HUB_PUBLIC_BASE_URL=changeme
+export HUB_INTERNAL_BASE_URL=changeme
+export HUB_DATALORE_SERVICE_ID=changeme
+export HUB_DATALORE_SERVICE_SECRET=changeme
+export HUB_PERM_TOKEN=changeme
+export HUB_FORCE_EMAIL_VERIFICATION=false
+
+export DEFAULT_INSTANCE_TYPE_ID=k8s-datalore-agent
+export ENABLE_PLANS=false
+export DEFAULT_PACKAGE_MANAGER=conda
+export DEFAULT_BASE_ENV_NAME=minimal
+
+export MAIL_ENABLED=false # if set to true, fill the fields below
+export MAIL_SENDER_EMAIL=
+export MAIL_SENDER_NAME=
+export MAIL_SENDER_USERNAME=
+export MAIL_SENDER_PASSWORD=
+export MAIL_SMTP_SERVER=
+export MAIL_SMTP_PORT=
