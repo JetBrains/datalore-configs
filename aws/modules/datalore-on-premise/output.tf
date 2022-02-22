7d9b67c7f1784cbff33055dc8cdb2f391f458c17
Aleksandr Borisenko <aleksandr.borisenko@jetbrains.com>
2022-02-22 15:41:49 +0300

    fix terraform variables

--- aws/modules/datalore-on-premise/output.tf
+++ aws/modules/datalore-on-premise/output.tf
@@ -8,10 +8,10 @@ output "datalore_ip" {
   value = aws_instance.datalore.public_ip
 }
 output "db_endpoint" {
-  value = aws_db_instance.datalore.endpoint
+  value = aws_db_instance.datalore[0].endpoint
 }
 output "db_password" {
-  value = aws_db_instance.datalore.password
+  value = aws_db_instance.datalore[0].password
 }
 output "datalore_user" {
   value = "${aws_iam_access_key.datalore.id} ${aws_iam_access_key.datalore.secret}"
@@ -23,5 +23,5 @@ output "registry_url" {
   value = "${aws_ecr_repository.computation-agent.registry_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.name_prefix}"
 }
 output "init_script" {
-  value = "./datalore.sh --public-ip-address ${aws_instance.datalore.public_ip} --internal-ip-address ${aws_instance.datalore.private_ip} --db-host ${aws_db_instance.datalore.address} --db-password ${aws_db_instance.datalore.password} --aws-access-key ${aws_iam_access_key.datalore.id} --aws-access-secret ${aws_iam_access_key.datalore.secret} --keypair-name ${var.ssh_keypair} --s3-environments-address ${aws_s3_bucket.envs.id} --docker-registry-address ${aws_ecr_repository.computation-agent.registry_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.name_prefix} --agent-iam-profile ${aws_iam_instance_profile.agent.name} --agent-subnet-id ${aws_subnet.agents.id} --security-group-id ${aws_security_group.agents.id} --availability-zone-id ${var.datalore_az} --aws-region ${data.aws_region.current.name} init"
+  value = "./datalore.sh --public-ip-address ${aws_instance.datalore.public_ip} --internal-ip-address ${aws_instance.datalore.private_ip} --db-host ${aws_db_instance.datalore[0].address} --db-password ${aws_db_instance.datalore[0].password} --aws-access-key ${aws_iam_access_key.datalore.id} --aws-access-secret ${aws_iam_access_key.datalore.secret} --keypair-name ${var.ssh_keypair} --s3-environments-address ${aws_s3_bucket.envs.id} --docker-registry-address ${aws_ecr_repository.computation-agent.registry_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${var.name_prefix} --agent-iam-profile ${aws_iam_instance_profile.agent.name} --agent-subnet-id ${aws_subnet.agents.id} --security-group-id ${aws_security_group.agents.id} --availability-zone-id ${var.datalore_az != null ? var.datalore_az : data.aws_availability_zones.available.names[0]} --aws-region ${data.aws_region.current.name} init"
 }
