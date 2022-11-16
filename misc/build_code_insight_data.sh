7dbe5007b0bc48bfa4de0e378a59dec49aad3be5
Artem Borzilov <artem.borzilov@jetbrains.com>
2022-11-16 23:10:47 +0400

    update build_code_insight_data.sh to match 2022.3 version

--- misc/build_code_insight_data.sh
+++ misc/build_code_insight_data.sh
@@ -11,13 +11,7 @@ fi
 SYSTEM_DIR=${SYSTEM_DIR:-/opt/python_analysis_tool}
 mkdir -p "$SYSTEM_DIR"
 
-JAVA_OPTS="\
--Didea.system.path=\"$SYSTEM_DIR\" \
-$JIGSAW_JAVA_OPTS \
--cp ${CODE_SERVER_LIB_DIR}/* \
-"
+JAVA_OPTS="-Didea.system.path=\"$SYSTEM_DIR\" $JIGSAW_JAVA_OPTS"
 
-set -xf
-
-eval "java $JAVA_OPTS jetbrains.datalore.notebook.server.analysisServer.Main python_skeleton_generator $1"
-eval "java $JAVA_OPTS jetbrains.datalore.notebook.server.analysisServer.Main indexer $1"
+eval "java -jar $JAVA_OPTS PythonAnalysisServer_deploy.jar python_skeleton_generator $1"
+eval "java -jar $JAVA_OPTS PythonAnalysisServer_deploy.jar indexer $1"
